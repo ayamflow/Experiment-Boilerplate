a7dd64357df2784d3f71f5e6dead3c9bdd0f59b6
Florian Morel <ayamflow@gmail.com>
2013-10-13 13:32:28 +0200

    Removed Pixi.js for now, added GruntJS for quick static server, added Math, Mouse and LeapBridge helpers. Turned Resize to a singleton with a resizables array. Added a basic object pool.

---
+++ src/experiment/helpers/Resize.js
@@ -0,0 +1,33 @@
+define([], function() {
+
+    var Resize = function() {
+        this.screenWidth = window.innerWidth;
+        this.screenHeight = window.innerHeight;
+        this.halfScreenWidth = this.screenWidth >> 1;
+        this.halfScreenHeight = this.screenHeight >> 1;
+
+        // A list of elements to resize
+        this.resizables = [];
+
+        window.addEventListener('resize', this.onResize.bind(this));
+    };
+
+    Resize.prototype = {
+        onResize: function() {
+            this.screenWidth = window.innerWidth;
+            this.screenHeight = window.innerHeight;
+            this.halfScreenWidth = this.screenWidth >> 1;
+            this.halfScreenHeight = this.screenHeight >> 1;
+
+            // Resize supplied elements
+            for(var i = 0; i < this.resizables.length; i++) {
+                this.resizables[i].width = this.screenWidth;
+                this.resizables[i].height = this.screenHeight;
+            }
+        }
+    };
+
+    var ResizeSingleton = new Resize();
+
+    return ResizeSingleton;
+});
